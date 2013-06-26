39690f3a63ec56fd1b0622df7fffae48d4ab8403
Roger Camps <rogercampsny@gmail.com>
2013-06-26 13:57:47 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dttools::Application.config.secret_token = 'f336fbbb562fa75bf47f43f4b2e177ddd778d1cdc290f166cfcd86f61f6a8823a957010885df8c46ff676f22a225b940c5569c9c9515a69c783916eb087b699c'
